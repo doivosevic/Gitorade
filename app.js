1a435b9be4d89a6c5143c4f5d7fd15e715e14f5c
Dominik Ivošević <dominik.dito@gmail.com>
2015-10-19 14:28:03 +0200

    Added crypto checking of secret

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var crypto = require('crypto')
+  , shasum = crypto.createHash('sha1');
 
+
+
+var rawLocalSecret = process.env.GITSECRET || 'testsecret';
+shasum.update(rawLocalSecret, 'utf8');
+var sha1LocalSecret = 'sha1=' + shasum.digest('hex');
 
 var app = express();
 
@@ -27,13 +34,24 @@ app.post('/git', function(req, res){
 	console.log('/git');
 	console.log(req.body);
 	console.log('\n\nand now header\n\n');
-	console.log(req.header);
-	console.log('\n\nand now header\n\n');
-	console.log(req);
-	var usefulData = {
-		
+	console.log(req.headers);
+
+	var sha1RemoteSecret = req.headers['x-hub-signature'];
+
+	console.log('\n\nand now header hook sig\n\n');
+	console.log(sha1RemoteSecret);
+
+	if (sha1RemoteSecret === sha1LocalSecret) {
+		var usefulData = {
+			
+		}
+		res.status(202).send('Hook accepted :)');
 	}
-	res.status(202).send('Hook accepted :)');
+	else {
+		console.log("Wrong secret. Ignoring commit!");
+		res.status(400).send('Wrong secret!! :(');
+		return;
+	}
 });
 
 module.exports = app;
