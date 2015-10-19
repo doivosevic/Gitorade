d6543d5073b3ba04f5fab0f8a2eac4a0eeece024
Dominik Ivošević <dominik.dito@gmail.com>
2015-10-19 15:00:18 +0200

    Fixed calculating and comparing hashed local and remote secrets

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var crypto = require('crypto')
-  , shasum = crypto.createHash('sha1');
+var crypto = require('crypto');
 
 
-
-var rawLocalSecret = process.env.GITSECRET || 'testsecret';
-shasum.update(rawLocalSecret, 'utf8');
-var sha1LocalSecret = 'sha1=' + shasum.digest('hex');
 
 var app = express();
-
 var config = fs.readFileSync('./gitorade.config', 'utf8');
-
 console.log(config);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
@@ -36,16 +28,23 @@ app.post('/git', function(req, res){
 	console.log('\n\nand now header\n\n');
 	console.log(req.headers);
 
+	//// CHECKING WEBHOOK SECRET
+	// CALCULATING FROM PAYLOAD AND LOCAL SECRET
+	var payload = req.body;
+	hmac.update(JSON.stringify(payload));
+	var sha1LocalSecret = 'sha1=' + hmac.digest('hex');
+	// GETTING REMOTE SECRET
 	var sha1RemoteSecret = req.headers['x-hub-signature'];
 
 	console.log('\n\nand now header hook sig\n\n');
 	console.log(sha1RemoteSecret);
+	console.log(sha1LocalSecret);
 
 	if (sha1RemoteSecret === sha1LocalSecret) {
 		var usefulData = {
 			
 		}
-		res.status(202).send('Hook accepted :)');
+		res.status(202).send('Secret ok. Hook accepted :)');
 	}
 	else {
 		console.log("Wrong secret. Ignoring commit!");
